93b6ee14554b9e23dd45ff1b3aaacbce50901018
Tudor Pasca <tudorpasca11@gmail.com>
2025-05-10 19:16:36 +0300

    Add AccountRepository

---
+++ include/repository/AccountRepository.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "IRepository.h"
+#include "../aggregate/AccountAggregate.h"
+
+class AccountRepository : public IRepository, public IEventVisitor {
+public:
+    void processEvent(const IDomainEvent &event) override;
+
+    void visit(const OrderCancelled &event) override;
+
+    void visit(const OrderPlaced &event) override;
+
+    void visit(const FundsDebited &event) override;
+
+    void visit(const FundsCredited &event) override;
+
+    void visit(const TradeExecuted &event) override;
+
+private:
+    std::unordered_map<int, AccountAggregate> accounts;
+};
---
+++ source/repository/AccountRepository.cpp
@@ -0,0 +1,48 @@
+#include "../../include/repository/AccountRepository.h"
+
+#include "../../include/event/FundsCredited.h"
+#include "../../include/event/FundsDebited.h"
+#include "../../include/event/TradeExecuted.h"
+
+void AccountRepository::processEvent(const IDomainEvent &event) {
+    event.acceptVisitor(*this);
+}
+
+void AccountRepository::visit(const FundsCredited &event) {
+    int clientId = event.getUserId();
+    if (accounts.find(clientId) == accounts.end()) {
+        accounts[clientId] = AccountAggregate(clientId);
+    }
+    accounts[clientId].addBalance(event.getQuantity());
+}
+
+void AccountRepository::visit(const FundsDebited &event) {
+    int clientId = event.getUserId();
+    double quantity = event.getQuantity();
+    if (accounts.find(clientId) == accounts.end() || accounts[clientId].getBalance() < quantity) {
+        throw std::runtime_error("[AccountRepository] Withdrawal failed. Insufficient funds for client " + clientId);
+    }
+    accounts[clientId].subtractBalance(quantity);
+}
+
+void AccountRepository::visit(const TradeExecuted &event) {
+    int buyerId = event.getBuyerId();
+    int sellerId = event.getSellerId();
+    double totalPrice = event.getQuantity() * event.getPrice();
+    if (accounts.find(buyerId) == accounts.end() || accounts[buyerId].getBalance() < totalPrice) {
+        throw std::runtime_error("[AccountRepository] Transaction failed. Insufficient funds for buyer " + buyerId);
+    }
+    if (accounts.find(sellerId) == accounts.end()) {
+        accounts[sellerId] = AccountAggregate(sellerId);
+    }
+    accounts[sellerId].addBalance(totalPrice);
+    accounts[buyerId].subtractBalance(totalPrice);
+}
+
+
+void AccountRepository::visit(const OrderCancelled &event) {}
+
+void AccountRepository::visit(const OrderPlaced &event) {}
+
+
+
