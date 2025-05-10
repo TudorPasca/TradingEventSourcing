80a8ca8757a04287243a309ba48c9188aa444a0f
Tudor Pasca <tudorpasca11@gmail.com>
2025-05-10 17:35:24 +0300

    Add placeOrder function to OrderBookAggregate

--- include/aggregate/OrderBookAggregate.h
+++ include/aggregate/OrderBookAggregate.h
@@ -11,10 +11,10 @@
 class OrderBookAggregate: IAggregate {
 public:
     OrderBookAggregate();
-    void placeOrder(std::shared_ptr<Order::IActiveOrder> order);
+    void placeOrder(const std::shared_ptr<Order::IActiveOrder>& order);
     std::shared_ptr<Order::IActiveOrder> getTopBuyOrder(const std::string& symbol) const;
     std::shared_ptr<Order::IActiveOrder> getTopSellOrder(const std::string& symbol) const;
 private:
-    std::unordered_set<Order::IActiveOrder> all_orders_;
+    std::unordered_set<std::shared_ptr<Order::IActiveOrder>> all_orders_;
     std::unordered_map<std::string, std::priority_queue<std::shared_ptr<Order::IActiveOrder>>> buy_orders_by_symbol_, sell_orders_by_symbol_;
 };
--- source/aggregate/OrderBookAggregate.cpp
+++ source/aggregate/OrderBookAggregate.cpp
@@ -3,6 +3,19 @@
 OrderBookAggregate::OrderBookAggregate()
     : all_orders_(), buy_orders_by_symbol_(), sell_orders_by_symbol_() {}
 
+void OrderBookAggregate::placeOrder(const std::shared_ptr<Order::IActiveOrder>& order) {
+    if (order == nullptr) {
+        return;
+    }
+    all_orders_.insert(order);
+    if (order->getType() == Order::OrderType::BUY) {
+        buy_orders_by_symbol_.at(order->getSymbol()).push(order);
+    } else {
+        sell_orders_by_symbol_.at(order->getSymbol()).push(order);
+    }
+}
+
+
 std::shared_ptr<Order::IActiveOrder> OrderBookAggregate::getTopBuyOrder(const std::string &symbol) const {
     return buy_orders_by_symbol_.at(symbol).top();
 }
