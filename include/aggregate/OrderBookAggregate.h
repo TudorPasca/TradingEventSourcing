c6c133da30829288d74adc07f4837e2db537e402
Tudor Pasca <tudorpasca11@gmail.com>
2025-05-11 17:17:24 +0300

    Refactor OrderBookAggregate to efficiently handle order cancelling

--- include/aggregate/OrderBookAggregate.h
+++ include/aggregate/OrderBookAggregate.h
@@ -4,17 +4,44 @@
 
 #include "IAggregate.h"
 #include "../order/IActiveOrder.h"
+#include "../order/BuyOrder.h"
+#include "../order/SellOrder.h"
 #include <queue>
+#include <set>
 #include <unordered_map>
 #include <unordered_set>
 
-class OrderBookAggregate: IAggregate {
+
+inline const auto buyOrderComparator = [](const std::shared_ptr<Order::IActiveOrder> &a,
+                                          const std::shared_ptr<Order::IActiveOrder> &b) -> bool {
+    if (a == nullptr || b == nullptr)
+        return (a == nullptr) && (b != nullptr);
+    return a->getPrice() > b->getPrice();
+};
+
+inline const auto sellOrderComparator = [](const std::shared_ptr<Order::IActiveOrder> &a,
+                                           const std::shared_ptr<Order::IActiveOrder> &b) -> bool {
+    if (a == nullptr || b == nullptr)
+        return (a == nullptr) && (b != nullptr);
+    return a->getPrice() < b->getPrice();
+};
+
+class OrderBookAggregate : IAggregate {
 public:
     OrderBookAggregate();
-    void placeOrder(const std::shared_ptr<Order::IActiveOrder>& order);
-    std::shared_ptr<Order::IActiveOrder> getTopBuyOrder(const std::string& symbol) const;
-    std::shared_ptr<Order::IActiveOrder> getTopSellOrder(const std::string& symbol) const;
+
+    void placeOrder(const std::shared_ptr<Order::IActiveOrder> &order);
+
+    void cancelOrder(const std::shared_ptr<Order::IActiveOrder> &order);
+
+    std::shared_ptr<Order::IActiveOrder> getTopBuyOrder(const std::string &symbol) const;
+
+    std::shared_ptr<Order::IActiveOrder> getTopSellOrder(const std::string &symbol) const;
+
 private:
-    std::unordered_set<std::shared_ptr<Order::IActiveOrder>> all_orders_;
-    std::unordered_map<std::string, std::priority_queue<std::shared_ptr<Order::IActiveOrder>>> buy_orders_by_symbol_, sell_orders_by_symbol_;
+    using buyOrderSet = std::set<std::shared_ptr<Order::IActiveOrder>, decltype(buyOrderComparator)>;
+    using sellOrderSet = std::set<std::shared_ptr<Order::IActiveOrder>, decltype(sellOrderComparator)>;
+    std::unordered_set<std::shared_ptr<Order::IActiveOrder> > all_orders_;
+    std::unordered_map<std::string, buyOrderSet> buy_orders_by_symbol_;
+    std::unordered_map<std::string, sellOrderSet> sell_orders_by_symbol_;
 };
--- source/aggregate/OrderBookAggregate.cpp
+++ source/aggregate/OrderBookAggregate.cpp
@@ -9,17 +9,28 @@ void OrderBookAggregate::placeOrder(const std::shared_ptr<Order::IActiveOrder>& 
     }
     all_orders_.insert(order);
     if (order->getType() == Order::OrderType::BUY) {
-        buy_orders_by_symbol_.at(order->getSymbol()).push(order);
+        buy_orders_by_symbol_.at(order->getSymbol()).insert(order);
     } else {
-        sell_orders_by_symbol_.at(order->getSymbol()).push(order);
+        sell_orders_by_symbol_.at(order->getSymbol()).insert(order);
     }
 }
 
+void OrderBookAggregate::cancelOrder(const std::shared_ptr<Order::IActiveOrder> &order) {
+    if (order == nullptr) {
+        return;
+    }
+    all_orders_.erase(order);
+    if (order->getType() == Order::OrderType::BUY) {
+        buy_orders_by_symbol_.at(order->getSymbol()).erase(order);
+    } else {
+        sell_orders_by_symbol_.at(order->getSymbol()).erase(order);
+    }
+}
 
 std::shared_ptr<Order::IActiveOrder> OrderBookAggregate::getTopBuyOrder(const std::string &symbol) const {
-    return buy_orders_by_symbol_.at(symbol).top();
+    return *buy_orders_by_symbol_.at(symbol).begin();
 }
 
 std::shared_ptr<Order::IActiveOrder> OrderBookAggregate::getTopSellOrder(const std::string& symbol) const {
-    return sell_orders_by_symbol_.at(symbol).top();
+    return *sell_orders_by_symbol_.at(symbol).begin();
 }
