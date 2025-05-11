52a18bf5904134c4c3c16d9ef8faf85886a927fd
Tudor Pasca <tudorpasca11@gmail.com>
2025-05-11 18:22:27 +0300

    Remove virtual "<" operator from IActiveOrder

--- include/order/IActiveOrder.h
+++ include/order/IActiveOrder.h
@@ -7,6 +7,7 @@ namespace Order {
     class IActiveOrder {
     public:
         IActiveOrder(int user_id, std::string symbol, int quantity, double price);
+        virtual ~IActiveOrder() = default;
 
         int getUserId() const;
         std::string getSymbol() const;
@@ -14,7 +15,6 @@ namespace Order {
         double getPrice() const;
 
         virtual OrderType getType() const = 0;
-        virtual bool operator<(const IActiveOrder &other) const = 0;
     protected:
         int user_id_;
         std::string symbol_;
